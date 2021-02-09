ed4ba69766e8c8cf2b683d49ed870d6ae5c57181
alams <alams@LAPTOP-JT03DJBH>
2021-02-08 22:40:54 -0500

    #init Project

---
+++ WahooFitness/src/test/java/Utilities/ConfigFileReader.java
@@ -0,0 +1,9 @@
+package Utilities;
+
+public class ConfigFileReader {
+
+	public String getReportConfigPath() {
+		String reportConfigPath = System.getProperty("user.dir") + "\\src\\test\\java\\Configs\\extent-config.xml";
+		return reportConfigPath;
+	}
+}
